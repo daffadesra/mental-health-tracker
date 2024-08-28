649f136fa68217fad152a3d1981b0e1dfb28d962
daffadesra <desrad81@gmail.com>
2024-08-28 14:47:59 +0700

    Menambahkan link PWS

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5cd4@qiezy!vl+i9qx%3pf#j*@vctx7y*(imcxds!$r@8z2_+h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "daffa-desra-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
